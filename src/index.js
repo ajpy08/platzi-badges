2277a1bdcf15da081c5c0a563707ebccd4b6a652
Angel Javier Puc Yamá <angelpuc08@gmail.com>
2020-02-04 21:29:51 -0600

    RJX

--- src/index.js
+++ src/index.js
@@ -5,12 +5,38 @@
 
 // container.appendChild(element);
 
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-const element = <h1>Hello, Platzi Badges!</h1>;
+// const jsx = <h1>Hello, Platzi Badges!</h1>;
+// const element = React.createElement('a',
+// {href: 'https://platzi.com'}, 'Ir a Platzi');
 
-const container = document.getElementById('app');
+const name = "Draki";
+const sum = () => 3 + 3;
+// const element = React.createElement('h1', {}, `Hola soy ${name}`);
+// const jsx = <h1>Hola soy {name}</h1>
+// const jsx = <h1>Hola soy, {sum()}</h1>
+
+//////////////////ES LO MISMO///////////////////
+
+const jsx = (
+  <div>
+    <h1>Hola soy Javi</h1>
+    <p>Soy ISC</p>
+  </div>
+);
+
+const element = React.createElement(
+  "div",
+  {},
+  React.createElement("h1", {}, "Hola soy Javi"),
+  React.createElement("p", {}, "Soy ISC")
+);
+
+////////////////////////////////////////////////
+
+const container = document.getElementById("app");
 
 // ReactDOM.render(__qué__, __dónde__);
 ReactDOM.render(element, container);
