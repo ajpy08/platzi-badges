b5e7d8cfb60fbee0fcad87c4f164ab6adccd1188
Angel Javier Puc Yamá <angelpuc08@gmail.com>
2020-02-09 10:25:24 -0600

    Enlazando Eventos

--- src/pages/BadgeNew.js
+++ src/pages/BadgeNew.js
@@ -1,28 +1,38 @@
-import React from 'react';
-import Navbar from '../components/Navbar'
-import logo from '../images/badge-header.svg'
-import './styles/BadgeNew.css'
-import Badge from '../components/Badge'
+import React from "react";
+import Navbar from "../components/Navbar";
+import header from "../images/badge-header.svg";
+import "./styles/BadgeNew.css";
+import Badge from "../components/Badge";
+import BadgeForm from "../components/BadgeForm";
 
 class BadgeNew extends React.Component {
-    render() {
-        return (
-            <div>
-                <Navbar></Navbar>
-                <div className="BadgeNew__hero">
-                    <img className="img-fluid" src={logo} alt="Logo" />
-                </div>
-
-                .<div className="container">
-                    <div className="row">
-                        <div className="col">
-                            <Badge firstName="Javi" lastName="Puc" twitter="ajpy08" jobTitle="Engineer" avatarURL="https://es.gravatar.com/avatar?d=identicon"></Badge>
-                        </div>
-                    </div>
-                </div>
+  render() {
+    return (
+      <div>
+        <Navbar></Navbar>
+        <div className="BadgeNew__hero">
+          <img className="img-fluid" src={header} alt="Logo" />
+        </div>
+        .
+        <div className="container">
+          <div className="row">
+            <div className="col-6">
+              <Badge
+                firstName="Javi"
+                lastName="Puc"
+                twitter="ajpy08"
+                jobTitle="Engineer"
+                avatarURL="https://secure.gravatar.com/avatar/7bb83a5a6a3e26948f3300581473134f"
+              ></Badge>
             </div>
-        )
-    }
+            <div className="col-6">
+              <BadgeForm />
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default BadgeNew;
